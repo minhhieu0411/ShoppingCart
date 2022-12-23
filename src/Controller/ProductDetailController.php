dafd0ce0b064d40b90a8612f72226900ad917dc5
minhhieu0411 <120078681+minhhieu0411@users.noreply.github.com>
2022-12-24 01:14:43 +0700

    Product detail

--- src/Controller/ProductDetailController.php
+++ src/Controller/ProductDetailController.php
@@ -5,14 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 
 class ProductDetailController extends AbstractController
 {
-    #[Route('/home/detail/{id}', name: 'product_detail')]
-    public function index(): Response
+    /**
+     * @Route("/home/{id}", name="home.detail")
+     */
+    public function index(Product $product): Response
     {
         return $this->render('product_detail/index.html.twig', [
-            'controller_name' => 'ProductDetailController',
+            'product' => $product,
         ]);
     }
 }
--- src/Entity/Account.php
+++ src/Entity/Account.php
@@ -4,23 +4,30 @@ namespace App\Entity;
 
 use App\Repository\AccountRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: AccountRepository::class)]
-class Account
+#[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
+class Account implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 180, unique: true)]
     private ?string $username = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $password = null;
+    #[ORM\Column]
+    private array $roles = [];
 
-    #[ORM\Column(length: 50)]
-    private ?string $usertype = null;
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
 
     public function getId(): ?int
     {
@@ -39,7 +46,39 @@ class Account
         return $this;
     }
 
-    public function getPassword(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
     {
         return $this->password;
     }
@@ -51,15 +90,12 @@ class Account
         return $this;
     }
 
-    public function getUsertype(): ?string
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
     {
-        return $this->usertype;
-    }
-
-    public function setUsertype(string $usertype): self
-    {
-        $this->usertype = $usertype;
-
-        return $this;
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
